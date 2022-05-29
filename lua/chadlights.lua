9b38f40b1279f5b08a3aa17e43b96a1d17eddfc7
Tavo Annus <tavo.annus@gmail.com>
2022-05-29 20:01:50 +0300

    Remove reverse dependecy

--- lua/chadlights.lua
+++ lua/chadlights.lua
@@ -1,5 +1,3 @@
-local ui = require("core.utils").load_config().ui
-
 local merge_tb = require("base46").merge_tb
 
 local highlights = {}
@@ -21,29 +19,6 @@ local polish_hl = require("base46").get_colors "polish_hl"
 
 if polish_hl then
    highlights = merge_tb(highlights, polish_hl)
-end
-
--- override user highlights if there are any
-if ui.hl_override then
-   local user_highlights = ui.hl_override
-   local colors = require("base46").get_colors "base_30"
-
-   -- fg = "white" set by user becomes fg = colors["white"]
-   -- so no need for the user to import colors
-
-   for group, _ in pairs(user_highlights) do
-      for key, val in pairs(user_highlights[group]) do
-         if key == "fg" or key == "bg" then
-            if val:sub(1, 1) == "#" or val == "none" or val == "NONE" then
-               user_highlights[group][key] = val
-            else
-               user_highlights[group][key] = colors[val]
-            end
-         end
-      end
-   end
-
-   highlights = merge_tb(highlights, user_highlights)
 end
 
 -- local set_transparent = require("core.utils").load_config().ui.transparency
